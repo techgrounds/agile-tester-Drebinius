90f4a4109c4f24c04de89ab7e38ce7abf8b226c1
Nathan Bakema <derbderp@gmail.com>
2022-08-16 16:28:10 +0200

    EoD push
    string coolifiers

--- Agile-Java/java-opdrachten/First-try-basics/src/Test.java
+++ Agile-Java/java-opdrachten/First-try-basics/src/Test.java
@@ -2,20 +2,14 @@ public class Test {
 
     public static void main(String[] args) {
 
-//        String nummer = "1.5";
-//        String nummer2 = "2.6";
-//
-//        String uitkomst = nummer + nummer2;
-//
-//        System.out.println(uitkomst);
-        test3(5,6);
-//        String tekst ="hej, ik ben zin";
-//        double decimaal = 1.0;
-//        float floatsy = 1.0F;
+
+//        test3(5,6);
+//        test3(15350,104305);
+
 
     }
 
-    private static void test3(Integer nummer1, Integer nummer2){
-        int uitkomst = nummer1 + nummer2;
+    private static void test3(Integer a,Integer b){
+        int uitkomst = a + b;
         System.out.println(uitkomst);
-    }
+    }}
---
+++ Agile-Java/java-opdrachten/First-try-basics/src/prac.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class prac {
+    public static void main(String[] args) {
+        addCruiseControlForCool("correctness value+5");
+
+    }
+
+    public static void addCruiseControlForCool(String s) {
+        System.out.println(s.toUpperCase() + "!");
+    }
+
+
+
+
+}
