508e1dd27de944d12314b6824a08b5a88d593b91
Nathan Bakema <derbderp@gmail.com>
2022-08-16 15:06:34 +0200

    first java stuff

---
+++ Agile-Java/java-opdrachten/First-try-basics/src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+//        String nummer = "1.5";
+//        String nummer2 = "2.6";
+//
+//        String uitkomst = nummer + nummer2;
+//
+//        System.out.println(uitkomst);
+        test3(5,6);
+//        String tekst ="hej, ik ben zin";
+//        double decimaal = 1.0;
+//        float floatsy = 1.0F;
+
+    }
+
+    private static void test3(Integer nummer1, Integer nummer2){
+        int uitkomst = nummer1 + nummer2;
+        System.out.println(uitkomst);
+    }
