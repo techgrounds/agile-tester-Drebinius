5eb0baad2965e44dca999f1c8325d23a2f1a5855
Nathan Bakema <derbderp@gmail.com>
2022-08-18 15:00:14 +0200

    opdrachten java

---
+++ Agile-Java/java-opdrachten/First-try-basics/src/opdrachtVO.java
@@ -0,0 +1,14 @@
+public class opdrachtVO {
+
+//  error, incompatible types. string cannot be converted to integer.
+    public static void main(String[] args) {
+
+    addition("hallo daar", 8);
+
+    }
+
+    public static void addition(String a, int b) {
+        int uitkomst = a + b;
+        System.out.println(uitkomst);
+
+    }}
