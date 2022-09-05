986777e2b901fe1f09a884401f2b49eac9565540
Nathan Bakema <derbderp@gmail.com>
2022-09-05 13:09:23 +0200

    random name 1

---
+++ Agile-Java/java-opdrachten/First-try-basics/src/Ben.java
@@ -0,0 +1,11 @@
+public class Ben {
+
+//  error, incompatible types. string cannot be converted to integer.
+    public static void main(String[] args) {
+            String name  = "Ben";
+            int numberOfCats = 10;
+
+        System.out.println(Ben);
+    }
+
+}
