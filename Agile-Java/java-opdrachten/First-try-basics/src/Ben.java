b857ac98d225c0bac5fb08f5e6815e0ca1cb543e
Nathan Bakema <derbderp@gmail.com>
2022-09-09 09:00:53 +0200

    somejava

--- Agile-Java/java-opdrachten/First-try-basics/src/Ben.java
+++ Agile-Java/java-opdrachten/First-try-basics/src/Ben.java
@@ -1,30 +1,46 @@
-class Ben {
+public class Ben {
 
 //  error, incompatible types. string cannot be converted to integer.
     public static void main(String[] args) {
             String name  = "Ben";
-
-            int x = 6;
-
-            int numberOfCats = x;
+            int numberOfCats =7;
 
 //        System.out.println();
-            CatMan("Ben",x);
+            getIntro("Ben", numberOfCats);
     }
 
-    public static void CatMan(String name, int numberOfCats) {
-        if (name == "Ben"){
+    public static void getIntro(String name, int numberOfCats) {
+        int cats = numberOfCats;
+
+        if (cats == 3){
             System.out.println(
                     ("This is ")
                             + name
                             +(", ")
                             + name
                             +(" has ")
-                            + numberOfCats
-                            +(" cats. He must smell nice.")
+                            + cats
+                            +(" cats.")
             );
-        } else {
-            System.out.println("THIS ISNT BEN");
+        } else if (cats >=4 && cats <=6) {
+            System.out.println(
+                    ("This is ")
+                            + name
+                            +(", ")
+                            + name
+                            +(" has ")
+                            + cats
+                            +(" cats. Must smell nice."));
+
+        } else if (cats >=7) {
+            System.out.println(
+                    ("This is ")
+                            + name
+                            +(", ")
+                            + name
+                            +(" has ")
+                            + cats
+                            +(" cats. Must be hell."));
         }
     }
 }
---
+++ Agile-Java/java-opdrachten/catAdder/src/Ben.java
@@ -0,0 +1,6 @@
+public class Ben {
+    public static void main(String[] args) {
+        String name = "Ben";
+
+    }
+}
